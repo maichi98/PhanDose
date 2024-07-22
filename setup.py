433ab30ec3516314615e682bcc61eb137be099d0
maichi98 <elaichi.mohammed.1998@gmail.com>
2024-07-22 11:31:24 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-
-# requirements list :
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name="phandose",
-    version='1.0.0',
-    description="",
-    author="EL AICHI MOHAMMED",
-    author_email="mohammed.el-aichi@gustaveroussy.fr",
-    url="https://github.com/maichi09/PhanDose",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
