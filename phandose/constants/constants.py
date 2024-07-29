1f99fd8e612cc86cbb43801964f747c3d4bb7529
maichi98 <elaichi.mohammed.1998@gmail.com>
2024-07-29 18:46:52 +0200

    modified constants.py, added tests for constants

--- phandose/constants/constants.py
+++ phandose/constants/constants.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import platform
 
 __all__ = [
     "DIR_PHANTOM_LIBRARY",
@@ -6,7 +7,12 @@ __all__ = [
     "DIR_LOGS"
 ]
 
-DIR_PHANTOM_LIBRARY = "/home/maichi/.PhanDose/PhantomLib"
-DIR_WORKSPACE = "/home/maichi/.PhanDose/Workspace"
+DIR_PHANTOM_LIBRARY_LINUX = "/home/maichi/.PhanDose/PhantomLib"
+DIR_PHANTOM_LIBRARY_WINDOWS = fr"D:/PhanDose/PhantomLib"
+DIR_PHANTOM_LIBRARY = DIR_PHANTOM_LIBRARY_LINUX if platform.system() == "Linux" else DIR_PHANTOM_LIBRARY_WINDOWS
+
+DIR_WORKSPACE_LINUX = "/home/maichi/.PhanDose/Workspace"
+DIR_WORKSPACE_WINDOWS = fr"D:/PhanDose/Workspace"
+DIR_WORKSPACE = DIR_WORKSPACE_LINUX if platform.system() == "Linux" else DIR_WORKSPACE_WINDOWS
 
 DIR_LOGS = str(Path(__file__).parent.parent.parent / "logs")
